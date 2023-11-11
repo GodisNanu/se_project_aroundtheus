27830350d2f104116d2f1a8a557095759d5a2a0c
Godis Nanu <godisnanu@gmail.com>
2023-11-11 15:51:27 -0500

    final stage

--- scripts/index.js
+++ scripts/index.js
@@ -32,13 +32,46 @@ const initialCards = [
 
 const editButton = document.querySelector("#editButton");
 const editModal = document.querySelector("#editModal");
+const closeModal = document.querySelector("#closeModal");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = document.querySelector("#name-input");
+const profileDescriptionInput = document.querySelector("#description-input");
+const profileEditForm = editModal.querySelector(".modal__form");
+const cardList = document.querySelector(".card__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+function closePopup() {
+  editModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__text");
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  return cardElement;
+}
 
 editButton.addEventListener("click", () => {
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   editModal.classList.add("modal_opened");
 });
 
-const closeModal = document.querySelector("#closeModal");
+closeModal.addEventListener("click", closePopup);
 
-closeModal.addEventListener("click", () => {
-  editModal.classList.remove("modal_opened");
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.prepend(cardElement);
 });
