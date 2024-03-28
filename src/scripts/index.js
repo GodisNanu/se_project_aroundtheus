9a8594b2804315b2e49e72f99f2153a199cf07eb
Godis Nanu <godisnanu@gmail.com>
2024-03-28 16:22:50 -0400

    updating index.js to correct Dom issues

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+import * as constants from "../utils/constants.js";
 import Card from "../components/Card.js";
 import { Section } from "../components/Section.js";
 import FormValidator from "../components/FormValidator.js";
@@ -5,55 +6,86 @@ import { PopupWithImage } from "../components/PopupWithImage.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 import "../pages/index.css";
-import * as constants from "../utils/constants.js";
 
 /* -------------------------------------------------------------------------- */
 /*                                  Elements                                  */
 /* -------------------------------------------------------------------------- */
+document.addEventListener("DOMContentLoaded", function () {
+  const addFormValidator = new FormValidator(
+    constants.formValidationConfig,
+    constants.addCardForm
+  );
 
-const addFormValidator = new FormValidator(
-  constants.formValidationConfig,
-  constants.addCardForm
-);
+  const editFormValidator = new FormValidator(
+    constants.formValidationConfig,
+    constants.profileEditForm
+  );
 
-const editFormValidator = new FormValidator(
-  constants.formValidationConfig,
-  constants.profileEditForm
-);
+  addFormValidator.enableValidation();
+  editFormValidator.enableValidation();
 
-addFormValidator.enableValidation();
-editFormValidator.enableValidation();
+  const newPopupImage = new PopupWithImage("#viewPicModal");
+  newPopupImage.setEventListeners("#viewPicModal");
 
-const newPopupImage = new PopupWithImage("#viewPicModal");
-newPopupImage.setEventListeners("#viewPicModal");
+  const editPopup = new PopupWithForm("#editModal", handleFormSubmit);
+  editPopup.setEventListeners("#editModal");
 
-const editPopup = new PopupWithForm("#editModal", handleFormSubmit);
-editPopup.setEventListeners("#editModal");
+  const cardPopup = new PopupWithForm("#addModal", handleFormSubmit);
+  cardPopup.setEventListeners("#addModal");
 
-const cardPopup = new PopupWithForm("#addModal", handleFormSubmit);
-cardPopup.setEventListeners("#addModal");
+  const newCardList = new Section(
+    { items: constants.initialCards, renderer: createCard },
+    ".cards__list"
+  );
 
-const newCardList = new Section(
-  { items: constants.initialCards, renderer: createCard },
-  ".cards__list"
-);
+  const useInfo = new UserInfo({
+    nameSelector: ".profile__title",
+    jobSelector: ".profile__description",
+  });
 
-const useInfo = new UserInfo({
-  nameSelector: ".profile__title",
-  jobSelector: ".profile__description",
+  newPopupImage.open("#viewPicModal");
+  handleImageClick(cardData);
+
+  /* -------------------------------------------------------------------------- */
+  /*                               Event Listeners                              */
+  /* -------------------------------------------------------------------------- */
+
+  constants.editButton.addEventListener("click", () => {
+    constants.profileNameInput.value = constants.profileName.textContent;
+    constants.profileDescriptionInput.value =
+      constants.profileDescription.textContent;
+    editPopup.open("#editModal");
+    editFormValidator.resetValidation();
+  });
+
+  constants.addButton.addEventListener("click", () => {
+    cardPopup.open("#addModal");
+  });
+
+  constants.addCardForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const addCard = {
+      name: constants.addCardTitleInput.value,
+      link: constants.addCardLinkInput.value,
+    };
+    getCard(addCard);
+    cardPopup.close("#addModal");
+    constants.addCardForm.reset();
+    addFormValidator.resetValidation();
+  });
+
+  newCardList.renderItems();
 });
 
 /* -------------------------------------------------------------------------- */
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
-
 function createCard(cardData) {
   const card = new Card(cardData, "#card-template", handleImageClick);
   return card.getView();
 }
 
 function handleImageClick(cardData) {
-  newPopupImage.open("#viewPicModal");
   constants.viewPicModalImage.src = cardData.link;
   constants.viewPicModalImage.alt = cardData.name;
   constants.viewPicModalTitle.textContent = cardData.name;
@@ -67,33 +99,3 @@ function handleFormSubmit(e) {
   useInfo.setUserInfo(nameValue, jobValue);
   editPopup.close("#editModal");
 }
-
-/* -------------------------------------------------------------------------- */
-/*                               Event Listeners                              */
-/* -------------------------------------------------------------------------- */
-
-editButton.addEventListener("click", () => {
-  constants.profileNameInput.value = constants.profileName.textContent;
-  constants.profileDescriptionInput.value =
-    constants.profileDescription.textContent;
-  editPopup.open("#editModal");
-  editFormValidator.resetValidation();
-});
-
-addButton.addEventListener("click", () => {
-  cardPopup.open("#addModal");
-});
-
-addCardForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const addCard = {
-    name: constants.addCardTitleInput.value,
-    link: constants.addCardLinkInput.value,
-  };
-  getCard(addCard);
-  cardPopup.close("#addModal");
-  constants.addCardForm.reset();
-  addFormValidator.resetValidation();
-});
-
-newCardList.renderItems();
