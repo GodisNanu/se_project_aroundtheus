b9b4818a75558a3d6b69e95c60331fe27e9d0d0e
Godis Nanu <godisnanu@gmail.com>
2024-04-18 12:21:57 -0400

    adding API endpoints

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,12 +1,13 @@
+import { JavascriptModulesPlugin } from "webpack";
+
 export default class Api {
-  constructor({ baseURl, headers }) {
-    this.server = baseURl;
+  constructor({ baseUrl, headers }) {
+    this.server = baseUrl;
     this.headers = headers;
-    console.log(baseURl);
   }
 
   getInitialCards() {
-    return fetch(this.server, {
+    return fetch(`${this.server}/cards`, {
       headers: this.headers,
     })
       .then((res) => {
@@ -19,17 +20,63 @@ export default class Api {
       });
   }
 
-  getUserInfo() {}
+  getUserInfo() {
+    return fetch(`${this.server}/users/me`, { headers: this.headers })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
-  updateProfileInfo() {}
+  updateProfileInfo() {
+    fetch(`${this.server}/users/me`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify({ name: "", about: "" }),
+    });
+  }
 
-  updateAvatar() {}
+  updateAvatar() {
+    fetch(`${this.server}/users/me/avatar`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify({ avatar: "" }),
+    });
+  }
 
-  createCards() {}
+  createCards() {
+    fetch(`${this.server}/cards`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify({ name: "", link: "" }),
+    });
+  }
 
-  deleteCards() {}
+  deleteCards(cardID) {
+    fetch(`${this.server}/cards/:${cardID}`, {
+      method: "DELETE",
+      headers: this.headers,
+      body: JSON.stringify({ name: "", link: "" }),
+    });
+  }
 
-  putCardLike() {}
+  putCardLike(cardID) {
+    fetch(`${this.server}/cards/:${cardID}/likes`, {
+      method: "PUT",
+      headers: this.headers,
+      body: JSON.stringify({ name: "", link: "", isLiked: true }),
+    });
+  }
 
-  deleteCardLike() {}
+  deleteCardLike(cardID) {
+    fetch(`${this.server}/cards/:${cardID}/likes`, {
+      method: "DELETE",
+      headers: this.headers,
+      body: JSON.stringify({ name: "", link: "", isLiked: true }),
+    });
+  }
 }
